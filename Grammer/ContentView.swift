738e91b58cc8811f3eec20add9e82ebba146650b
Scott Lougheed <scott@scottlougheed.com>
2020-07-09 14:12:01 -0700

    Initial Commit

---
+++ Grammer/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Grammer
+//
+//  Created by Scott Lougheed on 2020/07/9.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Grammer/GrammerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GrammerApp.swift
+//  Grammer
+//
+//  Created by Scott Lougheed on 2020/07/9.
+//
+
+import SwiftUI
+
+@main
+struct GrammerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
